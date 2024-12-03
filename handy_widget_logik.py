919cd9ac7a2e51c1c936067fd6f9c7710592ace2
Fjell247 <fjellviebahn@gmail.com>
2024-12-03 19:45:22 +0100

    Update handy_widget_logik.py

--- handy_widget_logik.py
+++ handy_widget_logik.py
@@ -10,6 +10,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+
+
 # Google Drive API-Setup
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 SERVICE_ACCOUNT_FILE = r"C:\Users\fjell\Desktop\Python\Keepn_Touch\Skripte\keepntouch-1a62cb9a0021.json"  # Dein Service Account Key
